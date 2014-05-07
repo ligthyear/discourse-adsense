f40b2d5eb99ca9affeff62e358aac2f6a7652d0a
Piioo <Piotr.Szal@googlemail.com>
2014-05-07 13:53:49 +0200

    rename adsense.js cos page do not load in development

--- plugin.rb
+++ plugin.rb
@@ -4,7 +4,7 @@
 # author: DiscourseHosting.com
 # minimum Discourse version: v0.9.9.3
 
-register_asset "javascripts/adsense.js"
+register_asset "javascripts/ad_views.js"
 
 register_asset "javascripts/injectors/discovery_injector.js.erb", :template_injector
 register_asset "javascripts/injectors/topic_injector.js.erb", :template_injector
