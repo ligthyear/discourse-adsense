deeaddfdef26e1ac53e85c35d3db3faa18fcdfe9
Benjamin Kampmann <ben.kampmann@gmail.com>
2014-05-24 12:14:33 +0200

    reload adsense JS every few times to make it realise we are on a new page

--- assets/javascripts/discourse/views/adsense_view.js
+++ assets/javascripts/discourse/views/adsense_view.js
@@ -1,10 +1,40 @@
-Discourse.AdsenseView = Discourse.View.extend({
-  didInsertElement: function() {
-    if(typeof adsbygoogle !== "undefined"){
-      try {
-        adsbygoogle.push({});
-      } catch (e) {}
+
+(function() {
+
+var ADS_COUNT = 0
+
+function __reload_gads (){
+    if (ADS_COUNT !== 0) {
+        ADS_COUNT -= 1;
+        return;
     }
 
+    ADS_COUNT = 3;
+    var ads = document.getElementById("adsense_loader");
+    if (ads){
+        // clear the old element and its state
+        ads.remove();
+        for (key in window) {
+            if (key.indexOf("google") !== -1){
+                window[key] = undefined;
+            }
+        }
+    }
+    window.adsbygoogle = [];
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.id="adsense_loader";
+    ga.src = '//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+}
+
+Discourse.AdsenseView = Discourse.View.extend({
+  didInsertElement: function() {
+    __reload_gads();
+    this.$("ins").each(function(idx, e) {
+            adsbygoogle.push({element: e});
+        });
   }
 });
+
+__reload_gads();
+
+})();
